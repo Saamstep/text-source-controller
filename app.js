187c9fce990fb080afc36cbabcd69784f6ce98db
Sam <Samstep2012@gmail.com>
2022-06-19 23:48:36 -0700

    ui done ish

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const { text } = require("body-parser");
 const express = require("express");
 const fs = require("node-fs").promises;
+// import express from "express";
+// import fs from "node-fs";
+// import fetch from "node-fetch";
+// import path from "path";
 const path = require("path");
 const app = express();
 app.use(express.json());
@@ -20,6 +23,60 @@ app.get("/update", async (req, res) => {
       if (err) console.error(err);
     });
   }
+  res.redirect(home);
+});
+
+app.get("/reset", async (req, res) => {
+  fs.writeFile(path.join(__dirname, textPath, "redScore" + ".txt"), `0`, (err) => {
+    if (err) console.error(err);
+  });
+  fs.writeFile(path.join(__dirname, textPath, "blueScore" + ".txt"), `0`, (err) => {
+    if (err) console.error(err);
+  });
+  res.redirect(home);
+});
+
+app.get("/swap", async (req, res) => {
+  let d = {};
+  let files = await fs.readdir(path.join(__dirname, textPath));
+  for (let i = 0; i < files.length; ++i) {
+    let content = await fs.readFile(path.join(__dirname, textPath, files[i]), "utf-8");
+    d[files[i].split(".")[0]] = content;
+  }
+
+  fs.writeFile(path.join(__dirname, textPath, "redScore" + ".txt"), `${d.blueScore}`, (err) => {
+    if (err) console.error(err);
+  });
+  fs.writeFile(path.join(__dirname, textPath, "blueScore" + ".txt"), `${d.redScore}`, (err) => {
+    if (err) console.error(err);
+  });
+  fs.writeFile(path.join(__dirname, textPath, "blueName" + ".txt"), `${d.redName}`, (err) => {
+    if (err) console.error(err);
+  });
+  fs.writeFile(path.join(__dirname, textPath, "redName" + ".txt"), `${d.blueName}`, (err) => {
+    if (err) console.error(err);
+  });
+
+  res.redirect(home);
+});
+
+app.get("/red/plus", async (req, res) => {
+  const score = await fs.readFile(path.join(__dirname, textPath, "redScore" + ".txt"));
+  let value = Number(score);
+  value = parseInt(value + 1);
+  fs.writeFile(path.join(__dirname, textPath, "redScore" + ".txt"), `${value}`, (err) => {
+    if (err) console.error(err);
+  });
+  res.redirect(home);
+});
+
+app.get("/blue/plus", async (req, res) => {
+  const score = await fs.readFile(path.join(__dirname, textPath, "blueScore" + ".txt"));
+  let value = Number(score);
+  value = parseInt(value + 1);
+  fs.writeFile(path.join(__dirname, textPath, "blueScore" + ".txt"), `${value}`, (err) => {
+    if (err) console.error(err);
+  });
   res.redirect(home);
 });
 
